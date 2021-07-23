2a44659475fb05434fd9e42565c38f3f4dd24bb3
Abu Shumon <johnshumon@gmail.com>
2021-07-23 17:25:38 +0100

    added user input and optimised code

--- randoms/prime_numbers.py
+++ randoms/prime_numbers.py
@@ -1,23 +1,43 @@
 """Checks prime number"""
 
 
-def is_prime_number(number: int) -> bool:
-    if number <= 1:
-        return False
-    else:
-        for element in range(2, number):
-            if number % element == 0:
-                return False
+def get_primes(limit: int = 1000000) -> list:
+    """Returns list of primes for a given limit"""
 
-        return True
+    # Return all between 2 to below 1
+    # million. Empty list otherwise
+    if limit < 2 or limit > 10 ** 6:
+        return []
+
+    prime_sieve = [True] * limit
+
+    # limit**0.5 -> square root of limit
+    # + 1 for celling value
+    for i in range(2, (int(limit ** 0.5) + 1)):
+        if prime_sieve[i]:
+            for j in range(i * i, limit, i):
+                prime_sieve[j] = False
+
+    return [prime for prime in range(2, limit) if prime_sieve[prime]]
+
+
+def is_prime_number(number: int) -> bool:
+    primes = get_primes()
+
+    return True if number in primes else False
 
 
 def main():
-    for i in range(100):
-        if is_prime_number(i):
-            print("{0}: prime".format(i))
-        else:
-            print("{0}: not prime".format(i))
+    input_number = int(input("Enter a number below 1000000 to see if it's a prime: "))
+
+    if is_prime_number(input_number):
+        print("{} is a prime".format(input_number))
+    else:
+        print("{} is not a prime".format(input_number))
+
+    input_number = int(input("Enter a number below 1000000: "))
+    primes = get_primes(input_number)
+    print("All prime below {} are: {}".format(input_number, primes))
 
 
 if __name__ == "__main__":
